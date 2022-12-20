9b8bf447daddd000431dc73276d9020660a01cdc
tintonajisadewo <32264817+tintonajisadewo@users.noreply.github.com>
2022-12-20 09:12:40 +0700

    [ADD] app deltatech_stock_date

---
+++ deltatech_stock_date/__manifest__.py
@@ -0,0 +1,25 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Stock Date",
+    "summary": "Set posting date for stock move",
+    "version": "16.0.1.0.2",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Warehouse",
+    "depends": ["base", "stock_account"],
+    "license": "LGPL-3",
+    "data": [
+        "wizard/stock_immediate_transfer_view.xml",
+        "wizard/stock_backorder_confirmation_view.xml",
+        "wizard/stock_picking_return_view.xml",
+        "data/ir_config_parameter.xml",
+    ],
+    "application": False,
+    "images": ["images/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
---
+++ deltatech_stock_date/models/stock.py
@@ -0,0 +1,64 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import api, fields, models
+
+
+class StockQuant(models.Model):
+    _inherit = "stock.quant"
+
+    @api.model
+    def _update_available_quantity(
+        self, product_id, location_id, quantity, lot_id=None, package_id=None, owner_id=None, in_date=None
+    ):
+        res = super(StockQuant, self)._update_available_quantity(
+            product_id, location_id, quantity, lot_id, package_id, owner_id, in_date
+        )
+
+        return res
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def write(self, vals):
+        date_fields = {"date", "date_expected"}
+        use_date = self.env.context.get("force_period_date", False)
+        if date_fields.intersection(vals):
+            if not use_date:
+                for move in self:
+                    today = fields.Date.today()
+                    if "date" in vals:
+                        date = fields.Date.to_date(vals["date"])
+                        move_date = fields.Date.to_date(move.date)
+                        if move_date < today and move_date < date:
+                            vals["date"] = move_date
+                        move.move_line_ids.write({"date": vals["date"]})
+            else:
+                if "date" in vals:
+                    vals["date"] = use_date
+
+        return super(StockMove, self).write(vals)
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def action_toggle_is_locked(self):
+        # se suprascrie metoda standard petnru a nu mai permite editarea
+        return False
+
+    # def button_validate(self):
+    #     if len(self) == 1:
+    #         return super(StockPicking, self.with_context(force_period_date=self.scheduled_date)).button_validate()
+    #     return super(StockPicking, self).button_validate()
+
+    def _action_done(self):
+        super(StockPicking, self)._action_done()
+        use_date = self.env.context.get("force_period_date", False)
+        if use_date:
+            self.write({"date": use_date, "date_done": use_date})
+            self.move_lines.write({"date": use_date})  # 'date_expected': use_date,
+            self.move_line_ids.write({"date": use_date})
